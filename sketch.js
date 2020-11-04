6b6339a430f86f1c6b17983a88010c4c86edddaf
sharminpavri <73242565+sharminpavri@users.noreply.github.com>
2020-11-04 21:06:56 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,29 @@
+var fixedRect,movingRect;
+
+function setup() {
+  createCanvas(800,600);
+ movingRect=createSprite(100,100,80,50);
+ fixedRect=createSprite(200,400,100,70);
+ movingRect.shapeColor="green";
+ fixedRect.shapeColor="green";  
+}
+
+function draw() {
+  
+  background(0);
+  movingRect.x=World.mouseX;
+  movingRect.y=World.mouseY;
+  if(movingRect.x-fixedRect.x<fixedRect.width/2+movingRect.width/2 &&
+    fixedRect.x-movingRect.x<fixedRect.width/2+movingRect.width/2&&
+    movingRect.y-fixedRect.y<fixedRect.height/2+movingRect.height/2 &&
+    fixedRect.y-movingRect.y<fixedRect.height/2+movingRect.height/2
+    ){
+    movingRect.shapeColor="red";
+    fixedRect.shapeColor="red";
+  } 
+  else{
+    movingRect.shapeColor="green";
+    fixedRect.shapeColor="green";
+  }
+  drawSprites();
+}
